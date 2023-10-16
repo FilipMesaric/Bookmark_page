cf44375b3b519a337312d865c56b3fe29dec7f08
Filip Mesarić <filip.mesaric@gmail.com>
2023-10-16 21:14:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+const apiUrl = "https://opengraph.io/api/1.1/site";
+const appId = "1afff298-029e-4b94-a4c1-2a4595cc036a";
+
+export default function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [url, setUrl] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const encodedUrl = encodeURIComponent(url);
+
+    // fetch a url from the opengraph.io api
+    fetch(`${apiUrl}/${encodedUrl}?app_id=${appId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const newBookmark = {
+          title: data.hybridGraph.title,
+          image: data.hybridGraph.image,
+          url: data.hybridGraph.url,
+        };
+
+        setBookmarks([...bookmarks, newBookmark]);
+      });
+
+    // add a bookmark to our bookmarks state variable
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* bookmarks form */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="https://example.com"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      {/* bookmarks list */}
+      <div id="container">
+        {bookmarks.map((bookmark, index) => (
+          <a
+            key={index}
+            href={bookmark.url}
+            className="bookmark"
+            target="_blank"
+          >
+            <img src={bookmark.image} alt="bookmark title" />
+            <span>{bookmark.title}</span>
+          </a>
+        ))}
+      </div>
+    </>
   );
 }
-
-export default App;
